5cb7c9e51d7679e2b00560e1c473dd0d13cbb047
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2016-12-27 12:39:29 +0900

    dlopen

--- src/CVVideo.jl
+++ src/CVVideo.jl
@@ -1,5 +1,19 @@
 module CVVideo
 
-# package code goes here
+using LibOpenCV
+using CVCore
+using Cxx
+
+libopencv_video = LibOpenCV.find_library_e("libopencv_video")
+try
+    Libdl.dlopen(libopencv_video, Libdl.RTLD_GLOBAL)
+catch e
+    warn("You might need to set DYLD_LIBRARY_PATH to load dependencies proeprty.")
+    rethrow(e)
+end
+
+cxx"""
+#include <opencv2/video.hpp>
+"""
 
 end # module
